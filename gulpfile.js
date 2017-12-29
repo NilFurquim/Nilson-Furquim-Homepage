c28cd2c286687697249509c0ee36bdab0740ab9c
NilFurquim <nilfurquimds@gmail.com>
2017-12-29 21:27:56 -0200

    project: init, gulpfile for sass, pug, and browserSync

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const pug = require('gulp-pug');
+const useref = require('gulp-useref');
+
+const browserSync = require('browser-sync').create();
+
+gulp.task('sass', function() {
+	return gulp.src('app/scss/**/*.scss')
+		.pipe(sass())
+		.pipe(gulp.dest('app/css'));
+});
+
+gulp.task('pug', function() {
+	return gulp.src('app/templates/**/*.pug')
+		.pipe(pug())
+		.pipe(gulp.dest('app/views'));
+});
+
+gulp.task('browserSync', function() {
+	browserSync.init({
+		server: {
+			baseDir: 'app'
+		},
+	});
+});
+
+gulp.task('watch', ['sass,', 'pug', 'browserSync'], function() {
+	gulp.watch('app/templates/**/*.pug', ['pug']);
+	gulp.watch('app/scss/**/*.scss', ['sass']);
+	gulp.watch('app/**/*.html', browserSync.reload());
+	gulp.watch('app/js/**/*.js', browserSync.reload());
+});
